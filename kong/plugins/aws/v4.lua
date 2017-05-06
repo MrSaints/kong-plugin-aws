6f1966f46fbc12f7a6b36d50fdad20d7a270e7bc
Ian Lai <ian@fyianlai.com>
2017-05-06 11:42:47 +0100

    Update AWS v4 signer to reflect upstream changes
    See ff38f5f in `Mashape/kong`:
    
    	ngx.time uses a cached time struct that obviates the need for the
    	syscall performed by os.time. additionally, os.time is not JITable.

--- kong/plugins/aws/v4.lua
+++ kong/plugins/aws/v4.lua
@@ -1,4 +1,4 @@
--- Taken from https://github.com/Mashape/kong/blob/master/kong/plugins/aws-lambda/v4.lua
+-- Taken, and modified from https://github.com/Mashape/kong/blob/master/kong/plugins/aws-lambda/v4.lua
 -- Performs AWSv4 Signing
 -- http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
 
@@ -110,7 +110,7 @@ local function prepare_awsv4_request(tbl)
       return nil, "either 'signing_key' or 'secret_key' must be provided"
     end
   end
-  local timestamp = tbl.timestamp or os.time()
+  local timestamp = tbl.timestamp or ngx.time()
   local tls = tbl.tls
   if tls == nil then tls = true end
   local port = tbl.port or (tls and 443 or 80)
